6b342137b7324200eb5b5c49bbf80f1aff5aec94
Osama-tec <hashmiosama555@gmail.com>
2023-10-11 00:00:15 +0500

    get profile with plan model

--- src/controllers/User/userData.js
+++ src/controllers/User/userData.js
@@ -39,7 +39,9 @@ const updateUserProfile = async (req, res) => {
 
 const getUserProfile = async (req, res) => {
   try {
-    const user = await User.findById(req.user.id).select('-password -_id');
+    const user = await User.findById(req.user.id)
+      .select('-password -_id')
+      .populate('plan'); 
 
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
@@ -50,6 +52,7 @@ const getUserProfile = async (req, res) => {
     return res.status(500).json({ error: error.message });
   }
 };
+
 
 
 
