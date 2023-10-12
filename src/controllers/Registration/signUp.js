21c1c91505336f4e3dae7493a05fed79c07f45f9
Osama-tec <hashmiosama555@gmail.com>
2023-10-12 22:54:47 +0500

    token added in sigunup

--- src/controllers/Registration/signUp.js
+++ src/controllers/Registration/signUp.js
@@ -1,5 +1,9 @@
 const User = require('../../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const saltRounds = 10;
 
 const signUp = async (req, res) => {
@@ -19,14 +23,16 @@ const signUp = async (req, res) => {
   }
 
   try {
-    // Check if a user with the same email already exists
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
+    // Check if a user with the same email or username already exists
+    const existingUserEmail = await User.findOne({ email });
+    const existingUserUsername = await User.findOne({ username });
+
+    if (existingUserEmail) {
       return res.status(400).json({ error: 'A user with this email already exists' });
     }
-    const existUser = await User.findOne({ username });
-    if (existUser) {
-      return res.status(400).json({ error: 'A user with this name already exists' });
+
+    if (existingUserUsername) {
+      return res.status(400).json({ error: 'A user with this username already exists' });
     }
 
     // Hash the password using bcrypt
@@ -39,7 +45,19 @@ const signUp = async (req, res) => {
     });
 
     await user.save();
-    res.status(201).json({ message: 'User created successfully' });
+
+    // Create token
+    const token = jwt.sign(
+        { id: user._id, username: user.username, email: user.email },
+        process.env.JWT_SECRET,
+        { expiresIn: "30d" }
+    );
+
+    // Save user token
+    user.token = token;
+
+    // Respond with token and user data
+    res.status(201).json({ message: 'User created successfully', TOKEN: token, user });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'An error occurred while creating the user' });
