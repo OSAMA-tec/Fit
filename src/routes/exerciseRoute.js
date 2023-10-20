5e523a87a666d99238ba757925c4b6d63668ff62
Osama-tec <hashmiosama555@gmail.com>
2023-10-20 23:03:33 +0500

    fixing exercise level

--- src/routes/exerciseRoute.js
+++ src/routes/exerciseRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {getAllExercises,getExercises,getExercisesByBodyPart,getExercisesByDayOfWeek} = require('../controllers/Exercises/getAll');
+const {getAllExercises,getExercises,getExercisesByBodyPart,getExercisesByDayOfWeek,getExercisesByLevel} = require('../controllers/Exercises/getAll');
 
 
 
