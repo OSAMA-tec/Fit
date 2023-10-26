d130ddb02c4123837bd477269d1b10fa46be4e6c
Osama-tec <hashmiosama555@gmail.com>
2023-10-26 21:59:59 +0500

    points added

--- src/controllers/User/userPoint.js
+++ src/controllers/User/userPoint.js
@@ -1,33 +1,30 @@
 const User = require('../../models/User'); 
 
-
 const addPoints = async (req, res) => {
   try {
-    // Check if points are provided
     if (!req.body.points) {
       return res.status(400).json({ error: 'Points are required' });
     }
 
-    // Check if user exists
+    const points = Number(req.body.points);
+    if (isNaN(points)) {
+      return res.status(400).json({ error: 'Invalid points value' });
+    }
+
     const user = await User.findById(req.user.id);
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
 
-    // Add points to the user
-    user.points += req.body.points;
+    user.points += points;
 
-    // Save the user
     await user.save();
 
-    // Send response
     res.status(200).json({ message: 'Points added successfully', user });
 
   } catch (error) {
-    // Handle error
     res.status(500).json({ error: 'An error occurred while adding points' });
   }
 };
-
 
 module.exports={addPoints}
