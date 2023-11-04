8abc37e39bee16d9ae027499b2ed103f8f0ba5e8
Osama-tec <hashmiosama555@gmail.com>
2023-11-04 22:47:46 +0500

    get all with plan

--- src/controllers/User/getAll.js
+++ src/controllers/User/getAll.js
@@ -1,12 +1,12 @@
 const User = require('../../models/User');
 
 const getNonAdminUsers =async (req, res) => {
-    try {
-        const users = await User.find({ role: { $ne: 'admin' }});
-        res.status(200).send(users);
-      } catch (err) {
-        res.status(500).send({ message: err.message });
-      }
+  try {
+    const users = await User.find({ role: { $ne: 'admin' }}).populate('plan');
+    res.status(200).send(users);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 };
 
 module.exports={getNonAdminUsers};
