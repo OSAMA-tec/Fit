50b5aaff812e821300511f812fe3fc5360272a49
Osama-tec <hashmiosama555@gmail.com>
2023-11-22 20:16:35 +0500

    verify added

--- src/routes/paymentRoute.js
+++ src/routes/paymentRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const {createPayment,executePayment,cancelPayment} = require('../controllers/Payment/payPal');
 const router = express.Router();
+const verifyToken = require('../middleware/auth');
 
-router.post('/payment/create',createPayment);
+router.post('/payment/create',verifyToken,createPayment);
 
-router.get('/payment/success', executePayment);
+router.get('/payment/success',verifyToken, executePayment);
 
-router.get('/payment/cancel', cancelPayment);
+router.get('/payment/cancel',verifyToken, cancelPayment);
 
 module.exports = router;
