680dcd7a438c79fb9bad4746f54edaaff17bea65
Osama-tec <hashmiosama555@gmail.com>
2023-11-23 10:38:18 +0500

    post cancel

--- src/routes/paymentRoute.js
+++ src/routes/paymentRoute.js
@@ -8,7 +8,7 @@ router.post('/payment/create',verifyToken,createPayment);
 
 router.get('/payment/success',verifyToken, executePayment);
 
-router.get('/payment/cancel',verifyToken, cancelPayment);
+router.post('/payment/cancel',verifyToken, cancelPayment);
 
 
 
