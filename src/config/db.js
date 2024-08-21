724a8fb2b38c927956fc798ff3b24bcb814f40b1
Osama-tec <hashmiosama555@gmail.com>
2024-08-21 06:55:37 +0500

    db

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const dbConnection = async() => {
+const dbConnection = async () => {
     try {
         await mongoose.connect(process.env.DB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-          
+
+
         });
         console.log('Database connected successfully');
     } catch (error) {
         console.log(`Error: ${error}`);
-        process.exit(1); 
+        process.exit(1);
     }
 };
 
